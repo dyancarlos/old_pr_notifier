1e9076bf73290993a3b73c5117fbeb2630693829
Dyan Carra <dyancarra@gmail.com>
2019-01-05 17:10:04 -0200

    Fix app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule App.MixProject do
 
   def project do
     [
-      app: :app,
+      app: :old_pr_notifier,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
