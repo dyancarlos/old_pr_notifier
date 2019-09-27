8c36dc2318635c7fa7aa5cc8359a2dace7a58f25
Dyan Carra <dyancarra@gmail.com>
2019-09-26 21:32:39 -0300

    only notify out of weekend

--- lib/old_pr_notifier/notifier.ex
+++ lib/old_pr_notifier/notifier.ex
@@ -1,9 +1,14 @@
 defmodule Notifier do
+  @saturday 6
+  @sunday 7
+
   def notify do
-    Filter.get
-    |> Enum.map(fn pr -> message(pr) end)
-    |> format_messages
-    |> send_notification
+    unless day_of_week == @saturday or day_of_week == @sunday do
+      Filter.get
+      |> Enum.map(fn pr -> message(pr) end)
+      |> format_messages
+      |> send_notification
+    end
   end
 
   defp send_notification(messages) do
@@ -17,5 +22,9 @@ defmodule Notifier do
 
   defp message(pr) do
     "The PR *#{pr.title}* (#{pr.url}) is waiting for review for the last *#{pr.days_ago}* days"
+  end
+
+  defp day_of_week do
+    DateTime.utc_now() |> Date.day_of_week()
   end
 end
